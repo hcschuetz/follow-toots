52e73008987f9411a335c764e8763a0c67819bd7
Heribert Schütz <hs@webxcerpt.com>
2026-01-04 14:02:04 +0100

    centralized navigation

---
+++ src/add-nav.ts
@@ -0,0 +1,6 @@
+document.querySelector("#nav")!.innerHTML = `
+<a href="about.html">About</a>
+<a href=".">Overview</a>
+<a href="config.html">Configuration</a>
+<a href="https://github.com/hcschuetz/follow-toots">Source</a>
+`;
