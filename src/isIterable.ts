3824735a69afd5b1454dae2f42533a35e072d7a3
Heribert Schütz <hs@webxcerpt.com>
2026-01-18 13:16:18 +0100

    minor generalization in isIterable

--- src/isIterable.ts
+++ src/isIterable.ts
@@ -1,2 +1,2 @@
 export default <T>(obj: T | Iterable<T>): obj is Iterable<T> =>
-  typeof (obj as Iterable<T>)[Symbol.iterator] === 'function';
+  typeof (obj as Iterable<T>)?.[Symbol.iterator] === 'function';
