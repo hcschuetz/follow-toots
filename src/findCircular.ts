99abe37e1449ca057e7119fe0eb93acbba69a89e
Heribert Schütz <hs@webxcerpt.com>
2026-01-19 01:45:25 +0100

    findCircular: changed an edge case
    ...which currently does not have an impact

--- src/findCircular.ts
+++ src/findCircular.ts
@@ -2,6 +2,7 @@
  * Find the first element in `ts` satisfying predicate `pred`,
  * starting the search just after element `pivot` and
  * continuing at the beginning of `ts` if necessary.
+ * If `pivot` is not in `ts`, just start at the beginning.
  * @returns the found element or `undefined`
  */
 export
@@ -9,7 +10,6 @@ function findCircular<T>(ts: Iterable<T>, pivot: T, pred: (t: T) => unknown): T 
   const array = ts instanceof Array ? ts : [...ts];
   const n = array.length;
   const pivotIdx = array.findIndex(t => t === pivot);
-  if (pivotIdx < 0) return; // or throw?
   let i = pivotIdx;
   do {
     i = (i + 1) % n;
@@ -22,6 +22,7 @@ function findCircular<T>(ts: Iterable<T>, pivot: T, pred: (t: T) => unknown): T 
  * Find the first element in `ts` satisfying predicate `pred`,
  * starting a backward search just before element `pivot` and
  * continuing at the end of `ts` if necessary.
+ * If `pivot` is not in `ts`, just start at the end.
  * @returns the found element or `undefined`
  * @throws a string if the pivot element cannot be found
  */
